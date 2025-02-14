0b23372d51de3c660a0bf85d78c8a11c81674783
itscris24 <cdavidsierra@unibarranquilla.edu.co>
2025-02-13 19:28:41 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let usuarios = [];
+
+// Cargar datos desde usuarios.json
+async function cargarUsuarios() {
+    try {
+        let respuesta = await fetch("../json/usuarios.json");
+        usuarios = await respuesta.json();
+    } catch (error) {
+        console.error("Error cargando los usuarios:", error);
+    }
+}
+
+// Llamar a la función para cargar usuarios al inicio
+cargarUsuarios();
+
+const ingresar = () => {
+    let usuarioInput = document.getElementById("Usuario").value;
+    let contraseñaInput = document.getElementById("Contraseña").value;
+
+    let usuarioEncontrado = usuarios.find(user => 
+        user.usuario === usuarioInput && user.contraseña === contraseñaInput
+    );
+
+    if (usuarioEncontrado) {
+        alert(`Bienvenido, ${usuarioEncontrado.usuario}. Rol: ${usuarioEncontrado.rol}`);
+        window.location.href = "../html/iniciouser.html"
+    } else {
+        alert("Usuario o contraseña incorrectos.");
+    }
+}
+
+const limpiar = () => {
+    document.getElementById("Usuario").value = "";
+    document.getElementById("Contraseña").value = "";
+}
