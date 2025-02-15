2675c4cc1da15befbad5727caae9f37abe45b8a6
itscris24 <cdavidsierra@unibarranquilla.edu.co>
2025-02-14 20:27:11 -0500

    Actualización de url

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let usuarios = [];
 // Cargar datos desde usuarios.json
 async function cargarUsuarios() {
     try {
-        let respuesta = await fetch("../json/usuarios.json");
+        let respuesta = await fetch("https://itscris24.github.io/proyecto/json/usuarios.json");
         usuarios = await respuesta.json();
         console.log("Usuarios cargados: ", usuarios)
     } catch (error) {
