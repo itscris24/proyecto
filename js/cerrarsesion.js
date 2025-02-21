239b4661eaeb0a165045fbf59f6888a99070bef7
itscris24 <cdavidsierra@unibarranquilla.edu.co>
2025-02-20 21:27:33 -0500

    Ultima actualización

--- js/cerrarsesion.js
+++ js/cerrarsesion.js
@@ -1,22 +1,4 @@
-
-const cerrarsesion = () => {
-    localStorage.removeItem("sesion");
-    window.location.href = "../index.html"; 
-};
-
-const bloquearRetroceso = () => {
-    history.pushState(null, "", location.href);
-    
-    window.addEventListener("popstate", () => {
-        let sesion = localStorage.getItem("sesionIniciada");
-        if (sesion) {
-            alert("⚠️ Debes cerrar sesión antes de salir.");
-            history.pushState(null, "", location.href); // Evita el retroceso
-        }
-    });
-};
-
-window.onload = () => {
-    bloquearRetroceso();
-};
-
+const cerrarSesion = () => {
+    localStorage.removeItem("usuario");
+    window.location.href = "../index.html"; // Redirigir al login
+}
